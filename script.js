458782ff1e9b1821a496cea5f70f7e631aab5e2f
Tasbia <tasbiauddin@gmail.com>
2023-09-02 16:42:40 -0400

    Submission

---
+++ script.js
@@ -0,0 +1,7 @@
+// Get a reference to the button element
+const button = document.getElementById('myButton');
+
+// Add a click event listener to the button
+button.addEventListener('click', function () {
+  button.textContent = 'Clicked!';
+});
